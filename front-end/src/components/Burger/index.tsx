718bdf23926f458d0ef25eec6187a838320e11cf
munyaaa <jessicajimantoro@gmail.com>
2019-11-16 14:48:42 +0700

    make burger x transformation

--- front-end/src/components/Burger/index.tsx
+++ front-end/src/components/Burger/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, CSSProperties } from 'react'
 import Style from './style.module.scss'
 import Menu from '../Menu'
 
@@ -7,12 +7,17 @@ export default () => {
 
   const toggleMenu: VoidFunction = () => setOpen(!isOpen)
 
+  const NormalTransform : CSSProperties = { transform : 'rotate(0deg)' }
+  const Transform45 : CSSProperties = { transform : 'rotate(45deg)' }
+  const TransformMin45 : CSSProperties = { transform : 'rotate(-45deg)' }
+  const Hidden : CSSProperties = { transform : 'rotate(0deg) scale(0)' } 
+
   return (
     <div className={Style.BurgerContainer}>
       <div className={Style.burger} onClick={toggleMenu}>
-        <span className={Style.burgerItem}></span>
-        <span className={Style.burgerItem}></span>
-        <span className={Style.burgerItem}></span>
+        <span className={Style.burgerItem} style={isOpen !== true ? NormalTransform : Transform45}></span>
+        <span className={Style.burgerItem} style={isOpen !== true ? NormalTransform : Hidden}></span>
+        <span className={Style.burgerItem} style={isOpen !== true ? NormalTransform : TransformMin45}></span>
       </div>
       <div> { isOpen && <Menu /> } </div>
     </div>
