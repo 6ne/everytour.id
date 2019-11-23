e0476f9feacf3de2682f1e58e4c4d76d2927dd66
Clavin June <juneardoc@gmail.com>
2019-11-23 16:13:23 +0700

    add p. harapan images

--- front-end/src/pages/Destination/index.tsx
+++ front-end/src/pages/Destination/index.tsx
@@ -14,6 +14,16 @@ import ptidung from '../../assets/img/wisata-pulau-tidung.jpg'
 import Image from '../../components/Image'
 import sunrise from '../../assets/img/sunrise.png'
 
+import _harapan3 from '../../assets/img/harapan/harapan3.jpg'
+import _harapan7 from '../../assets/img/harapan/harapan7.jpg'
+import _harapan8 from '../../assets/img/harapan/harapan8.jpg'
+import _harapan9 from '../../assets/img/harapan/harapan9.jpg'
+import _harapan10 from '../../assets/img/harapan/harapan10.jpg'
+import _harapan11 from '../../assets/img/harapan/harapan3.jpg'
+import _harapan12 from '../../assets/img/harapan/harapan11.jpg'
+import _harapan15 from '../../assets/img/harapan/harapan15.jpg'
+import _harapan19 from '../../assets/img/harapan/harapan19.jpg'
+
 export default () => {
   console.log(bunaken)
   let { name } = useParams();
@@ -42,7 +52,10 @@ export default () => {
   let imgList: Array<string> = []
 
   if (name === 'harapan') {
-    imgList = [sunset, pharapan, bunaken, sunset, pharapan, bunaken, sunset, pharapan, bunaken]
+    imgList = [
+      _harapan3, _harapan7, _harapan8, _harapan9, _harapan10, _harapan11,
+      _harapan12, _harapan15, _harapan19
+    ]
   } else if (name === 'tidung') {
     imgList = []
   } else if (name === 'pari') {
